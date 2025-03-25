b0e0e3577739f627605b20e933439d1b456afa60
DateTiro <a24i336jl@ous.jp>
2025-03-25 16:39:20 +0900

    worry

--- reversi.c
+++ reversi.c
@@ -7,11 +7,18 @@
 
 int board[HEIGHT][WIDTH];
 
-enum Object {
-    Empty,
+typedef enum Object {
     Dark,
     Light,
-};
+    Empty,
+} Object;
+
+bool check_lange(int y, int x) {
+    if (y < 0 || x < 0 || y >= HEIGHT || x >= WIDTH) {
+        return true;
+    }
+    return false;
+}
 
 void init_board() {
     for (int y = 0; y < HEIGHT; y++) {
@@ -24,7 +31,7 @@ void init_board() {
 void test_board() {
     for (int y = 0; y < HEIGHT; y++) {
         for (int x = 0; x < WIDTH; x++) {
-            board[y][x] = Light;
+            board[y][x] = Dark;
         }
     }
 }
@@ -55,7 +62,7 @@ void print_number() {
     }
 }
 
-void print_object(int n) {
+void print_object(Object n) {
     char a;
     if (n == Dark) {
         a = 'D';
@@ -79,36 +86,45 @@ void print_board() {
     print_number();
 }
 
+bool check_direction(int y, int x, Object col, int dy, int dx) {
+
+}
+
 /*
 Check if there are any flippable disks around.
 */
-bool check_around(int y, int x, int col) {
+bool check_around(int y, int x, Object col) {
+    int flag = 0, check[8][2] = {{1, -1}, {1, 0}, {1, 1}, {0, -1}, {0, 1}, {-1, -1}, {-1, 0}, {-1, 1}};
     for (int yy = y - 1; yy <= y + 1; yy++) {
         for (int xx = x - 1; xx <= x + 1; xx++) {
-            if (board[yy][xx] == col) {
-                return true;
+            if (yy != y && xx != x) {
+                flag++;
+                if (board[yy][xx] == col) {
+                    if (check_direction(yy, xx, col, check[flag][0], check[flag][1])) {
+                        return true;
+                    }
+                }
             }
         }
     }
     return false;
 }
 
-bool judge_disks(int y, int x) {
-    if (board[y][x] != Empty) {
-        if (board[y][x] == Dark) {
+bool judge_put_disks(int y, int x, Object col) {
+    if (check_lange(y, x)) {
+        if (col == Dark) {
             if (check_around(y, x, Light)) {
-
-            }
-        } else {
-            if (check_around(y, x, Dark)) {
-                
             }
         }
+
+        if (col == Light) {
+
+        }
     }
-    return false;
 }
 
 int main(void) {
+    // printf("Size of enum Object: %zu bytes\n", sizeof(Object)); 4bytes
     init_board();   
     place_object_first();
     // test_board();    // test
